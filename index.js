8d465c9d2e7b62bab15b54e031b109846c8b70c7
LalaChristl <lesleyannchristl@gmail.com>
2022-08-17 14:48:00 +0200

    added ks

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 //Write a function filterRange(arr, a, b) that gets an array arr, looks for elements with values higher or equal to a and lower or equal to b and return a result as an array.
 //1
 
+
 function filterRange(arr, a, b) {
-    //write your code here
+  return arr.filter(item => (a <= item && item <= b));
   }
   
   let arr = [5, 3, 8, 1];
@@ -13,14 +14,20 @@ function filterRange(arr, a, b) {
   
   console.log( arr ); // 5,3,8,1 (not modified)
 
+  console.log("----------------------------------------");
+
 //Write a function filterRangeInPlace(arr, a, b) that gets an array arr and removes from it all values except those that are between a and b. The test is: a ≤ arr[i] ≤ b.
 //2
   function filterRangeInPlace(arr, a, b) {
-    //write your code here
+    let filtered = filterRange(arr, a, b);
+    arr.length = 0;
+    for (item of filtered) {
+      arr.push(item);
+    }
   }
   
   let array = [5, 3, 8, 1];
-  
+
   filterRangeInPlace(array, 1, 4); // removed the numbers except from 1 to 4
   
   console.log( array ); // [3, 1]
